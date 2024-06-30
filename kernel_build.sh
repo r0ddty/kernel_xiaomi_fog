ab5e890457cbe9d35a712a372e3b241c0f675899
r0ddty <r0d@ukr.net>
2024-06-30 14:35:15 +0300

    fog: kernel_build.sh: add PixelDrain API check Added prompt for removing out and clang directories

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,4 +1,7 @@
 export TZ='Europe/Kyiv'
+
+KERNNAME="Atlas"
+KERNVER="B1"
 BUILDDATE=$(date +%Y%m%d)
 # BUILDTIME=$(date +%H%M)
 
@@ -27,9 +30,56 @@ git clone --depth=1 https://github.com/r0ddty/AnyKernel3-680 -b master AnyKernel
 cp -R out/arch/arm64/boot/Image.gz AnyKernel3/Image.gz
 # Zip it and upload it
 cd AnyKernel3
-zip -r9 Atlas-B1-"$BUILDDATE" . -x ".git*" -x "README.md" -x "*.zip"
-curl -T Atlas-B1-"$BUILDDATE".zip https://pixeldrain.com/api/file/
-# finish
+zip -r9 "$KERNNAME"-"$KERNVER"-"$BUILDDATE" . -x ".git*" -x "README.md" -x "*.zip"
+
+# Will use go-pd as direct upload requires API key which i dont have ╮(─▽─)╭
+# curl -T Atlas-B1-"$BUILDDATE".zip https://pixeldrain.com/api/file/
+
+# Lemme move kernel zip to the root of source
+
+mv "$KERNNAME"-"$KERNVER"-"$BUILDDATE" ../
 cd ..
-rm -rf clang-llvm/ out/ AnyKernel3/
+
+rm go-pd*
+wget https://github.com/ManuelReschke/go-pd/releases/download/v1.5.0/go-pd_1.5.0_linux_amd64.tar.gz
+mv go-pd_1.5.0_linux_amd64.tar.gz go-pd.tar.gz
+tar -xf go-pd.tar.gz
+rm go-pd.tar.gz
+
+echo "Please, input your PixelDrain API key below"
+# Dayum, go-pd now requires API key as well ヽ(。_°)ノ
+echo "If you dont have one - just press Enter key, kernel $KERNNAME-$KERNVER-$BUILDDATE.zip is located on the root of your source"
+read KEY
+if [ -z "${KEY}" ];
+then
+	echo "The key is empty, passing"
+else
+	echo "Your key is $KEY"
+	echo "Uploading kernel to PixelDrain"
+	./go-pd upload "$KERNNAME"-"$KERNVER"-"$BUILDDATE".zip -k $KEY -v	
+fi
+
+echo "Cleaning up..."
+
+read -p "Would you like to remove out/ directory? (Yy/Nn)" yn
+
+case $yn in
+	[yY] ) echo "Removing out/ directory";
+		rm -rf out/;;
+	[nN] ) echo "Proceeding without removing out/ directory";; 
+
+	* ) echo "invalid response";;
+esac
+
+read -p "Would you like to remove clang directory? (Yy/Nn)" yn
+
+case $yn in
+	[yY] ) echo "Removing clang directory";
+		rm -rf out/;;
+	[nN] ) echo "Proceeding without removing clang directory";; 
+
+	* ) echo "invalid response";;
+esac
+
+rm -rf AnyKernel3/
 echo "Build finished"
